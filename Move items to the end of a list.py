836715fa31306a652a559a39d0de009c50cc4ca5
Chris <72232622+chrisdk95@users.noreply.github.com>
2022-09-30 00:11:40 -0700

    Update Move items to the end of a list.py

--- Move items to the end of a list.py
+++ Move items to the end of a list.py
@@ -5,14 +5,14 @@
 def move_to_end(lst, val):
   result = []
   if lst == []:
-    return result
+    return result #if list is empty, return empty list
   if lst[0] == val:
-    result += move_to_end(lst[1:], val)
+    result += move_to_end(lst[1:], val) #use lst[1:] as the argument here in order to bring the input closer to the base case
+    result.append(lst[0]) #if first item matches val then we extract the first item from lst and append it to the end of the result  
+   else:
     result.append(lst[0])
-  else:
-    result.append(lst[0])
-    result += move_to_end(lst[1:], val)
-  return result
+    result += move_to_end(lst[1:], val) #where the first item does NOT match val, we need to extract the first item from lst and append it to the beginning of result.
+  return result # result will be a copy of lst with every instance of val moved to the end of the list.
 
 #or
 # def move_to_end(lst, val):
